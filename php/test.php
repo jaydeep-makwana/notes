6c674ad958a33272eff3b91cfdb19e113dfe1b65
jaydeep-makwana <jaydeepmakwana570@gmail.com>
2022-05-07 00:28:28 +0530

    first commit

---
+++ php/test.php
@@ -0,0 +1,27 @@
+<?php
+ 
+ $conn = mysqli_connect('localhost','root','','mydb');
+
+ 
+ 
+  
+
+// echo $a ;
+ 
+$query = "SELECT * FROM user";
+
+$result = mysqli_query($conn, $query);
+ 
+// echo mysqli_errno($conn);
+
+
+$assoc = mysqli_fetch_assoc($result);
+
+echo mysqli_error($conn);
+// echo mysqli_errno($conn);
+
+
+echo '<pre>';
+// var_dump($a);
+print_r($assoc);
+echo '</pre>';
